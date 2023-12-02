605aaecb9d508d7548a2e0c0a1b48c237b70aced
IluhaZaz <133998226+IluhaZaz@users.noreply.github.com>
2023-12-02 14:43:46 +0300

    Улучшена шейкерная сортировка, начата сортировка слиянием

--- sort.cpp
+++ sort.cpp
@@ -19,22 +19,119 @@ void bubble_sort(vector<T>& a) {
 
 
 template <typename T>
-void shaker_sort(vector<T>& a) {
-    int left_num = 1;
-    int right_num = a.size();
-    while (left_num < right_num) {
-        for (int i = left_num - 1; i < right_num - 1; i++) {
-            if (a[i] > a[i + 1]) {
-                swap(a[i], a[i + 1]);
-            }
-        }
-        right_num--;
+void reverse(vector<T>& a) {
+    for (int i = 0; i < a.size() / 2; i++) {
+        swap(a[i], a[a.size() - i - 1]);
+    }
+}
 
-        for (int i = right_num - 1; i > left_num - 1; i--) {
-            if (a[i] < a[i - 1]) {
-                swap(a[i], a[i - 1]);
+
+template <typename T>
+void shaker_sort(vector<T>& a, bool increase) {
+        int left_num = 1;
+        int right_num = a.size();
+        while (left_num < right_num) {
+            for (int i = left_num - 1; i < right_num - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    swap(a[i], a[i + 1]);
+                }
             }
+            right_num--;
+
+            for (int i = right_num - 1; i > left_num - 1; i--) {
+                if (a[i] < a[i - 1]) {
+                    swap(a[i], a[i - 1]);
+                }
+            }
+            left_num++;
         }
-        left_num++;
+    if(!increase) {
+        reverse(a);
+    }
+}
+
+
+template <typename T>
+void merge_sort_left(vector<T> a1, vector<T> a2, vector<T>& res_arr, int start_indx) {
+    shaker_sort(a1, 1);
+    shaker_sort(a2, 1);
+
+
+    int i = 0;
+    int j = 0;
+    int k = start_indx;
+
+    while (i < a1.size() && j < a2.size()) {
+        if (a1[i] <= a2[j]) {
+            res_arr[k] = a1[i];
+            k++;
+            i++;
+        }
+        else {
+            res_arr[k] = a2[j];
+            k++;
+            j++;
+        }
+    }
+    while (i < a1.size()) {
+        res_arr[k] = a1[i];
+        i++;
+        k++;
+    }
+    while (j < a2.size()) {
+        res_arr[k] = a2[j];
+        j++;
+        k++;
+    }
+}
+
+
+template <typename T>
+void merge_sort_right(vector<T> a1, vector<T> a2, vector<T>& res_arr, int end_indx) {
+    shaker_sort(a1, 0);
+    shaker_sort(a2, 0);
+
+
+    int i = 0;
+    int j = 0;
+    int k = end_indx;
+
+    while (i < a1.size() && j < a2.size()) {
+        if (a1[i] >= a2[j]) {
+            res_arr[k] = a1[i];
+            k--;
+            i++;
+        }
+        else {
+            res_arr[k] = a2[j];
+            k--;
+            j++;
+        }
+    }
+    while (i < a1.size()) {
+        res_arr[k] = a1[i];
+        i++;
+        k--;
+    }
+    while (j < a2.size()) {
+        res_arr[k] = a2[j];
+        j++;
+        k--;
+    }
+}
+
+
+template <typename T>
+void natural_merge_sort(vector<T> a) {
+    vector<T>b(a.size());
+
+    int a_start_indx = 0;
+    int a_end_indx = a.size() - 1;
+
+    int b_start_indx = 0;
+    int b_end_indx = b.size() - 1;
+    
+    while (a_start_indx < a_end_indx) {
+
     }
 }
--- sort_tests.cpp
+++ sort_tests.cpp
@@ -29,19 +29,58 @@ TEST(SortTests, ShakerSortCheck) {
     vector<int> a = { 1, 4, 3, 2 };
     vector<int> b = { 1, 2, 3, 4 };
 
-    shaker_sort(a);
+    shaker_sort(a, 1);
 
     EXPECT_EQ(a, b);
 }
+
 
 TEST(SortTests, ShakerSortCheck2) {
     vector<int> a = { 1, 2, 3, 4 };
     vector<int> b = { 1, 2, 3, 4 };
 
-    shaker_sort(a);
+    shaker_sort(a, 1);
 
     EXPECT_EQ(a, b);
 }
 
 
+TEST(SortTests, ShakerSortCheck3) {
+    vector<int> a = { 1, 2, 3, 4 };
+    vector<int> b = { 4, 3, 2, 1 };
 
+    shaker_sort(a, 0);
+
+    EXPECT_EQ(a, b);
+}
+
+
+TEST(SortTests, MergeSortLeftCheck) {
+    vector<int> a = { 1,8,5,6 };
+    vector<int> b = { 4, 2, 3, 7 };
+    vector<int> c(10);
+    c[0] = 10;
+    c[1] = 10;
+
+    merge_sort_left(a, b, c, 2);
+
+    vector<int> d = {10, 10, 1, 2, 3, 4, 5, 6, 7, 8 };
+    
+    EXPECT_EQ(d, c);
+    EXPECT_EQ(*b.data(), 4);
+}
+
+
+TEST(SortTests, MErgeSortRightCheck) {
+    vector<int> a = { 1,8,5,6 };
+    vector<int> b = { 4, 2, 3, 7 };
+    vector<int> c(10);
+    c[0] = 10;
+    c[1] = 10;
+
+    merge_sort_right(a, b, c, c.size() - 1);
+
+    vector<int> d = {10, 10, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+    EXPECT_EQ(d, c);
+}
